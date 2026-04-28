f64ad26abd03cafb8203496eeffbed0677809a0b
Ajay Patel <ajaypatelkota@gmail.com>
2026-04-28 23:05:58 +0530

    Customize error/exception and added Backend Validations

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,7 +33,7 @@ public class CategoryController {
     //@PostMapping("api/public/categories")
     //@RequestMapping(value="api/public/categories", method=RequestMethod.POST) //2-@RequestMapping at class
     @RequestMapping(value="/public/categories", method=RequestMethod.POST) //2-@RequestMapping at class
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
         categoryService.createCategory(category);
         //return "Category added successfully.";
         return new ResponseEntity<>("Category added successfully.",HttpStatus.CREATED);
