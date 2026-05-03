607e48165bc12a704660d20cfa4bc9e5eac26845
Ajay Patel <ajaypatelkota@gmail.com>
2026-05-03 22:39:45 +0530

    Validation added of size

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.message.Message;
 
 @Entity(name="categories")
 @Data
@@ -19,5 +22,6 @@ public class Category {
     private Long categoryId;
 
     @NotBlank
+    @Size(min=5, message="Category name should be of atleast 5 characters")
     private String categoryName;
 }
