e6f68af16e5f541e431217e5ddd12d60241a088c
Ajay Patel <ajaypatelkota@gmail.com>
2026-09-01 22:20:07 +0530

    Controller has been updated to take default Page_Number and Page_Size from config

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
@@ -28,8 +29,8 @@ public class CategoryController {
     //@RequestMapping(value="api/public/categories", method=RequestMethod.GET) //2-@RequestMapping at class
     @RequestMapping(value="/public/categories", method=RequestMethod.GET) //2-@RequestMapping at class
     public ResponseEntity<CategoryResponse>  getAllCategories(
-            @RequestParam(name="pageNumber")Integer pageNumber,
-            @RequestParam(name="pageSize")Integer pageSize
+            @RequestParam(name="pageNumber",defaultValue = AppConstants.PAGE_NUMBER, required = false)Integer pageNumber,
+            @RequestParam(name="pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false)Integer pageSize
     ){
     //public ResponseEntity<List<Category>> getAllCategories(){
         //return categoryService.getAllCategories();
